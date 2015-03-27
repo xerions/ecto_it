e265d9971f93e1f76b978b8544e959a3aac1be89
0xAX <kuleshovmail@gmail.com>
2015-03-27 20:08:27 +0600

    Added two repos for different databases

--- lib/ecto_it/repo.ex
+++ lib/ecto_it/repo.ex
@@ -1,3 +1,22 @@
 defmodule EctoIt.Repo do
-  use Ecto.Repo, otp_app: :ecto_it
+
+  def unquote(:"$handle_undefined_function")(program, args) do
+    Application.put_env(:ecto_it, EctoIt.get_repo(), adapter: EctoIt.get_adapter(), url: EctoIt.get_url(Mix.env))
+    EctoIt.get_adapter().storage_up(username: EctoIt.get_username(Mix.env), database: "ecto_test_default")
+    case Mix.env do
+      :pg ->
+        EctoIt.Repo.Postgres.start_link
+      _ ->
+        EctoIt.Repo.MySQL.start_link
+    end
+  end
+
+end
+
+defmodule EctoIt.Repo.Postgres do
+  use Ecto.Repo, otp_app: :ecto_it, adapter: Ecto.Adapters.Postgres
+end
+
+defmodule EctoIt.Repo.MySQL do
+  use Ecto.Repo, otp_app: :ecto_it, adapter: Ecto.Adapters.MySQL
 end
